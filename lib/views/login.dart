641ab451c68b519a47b0bc18a84496527200b92d
Jayson Brennan <jaysonbrennan@protonmail.com>
2022-01-17 18:29:55 -0500

    Creating login page shell

---
+++ lib/views/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'TODO: Login stuff',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      backgroundColor: Color(0xFF424242), // dark grey
+    );
+  }
+}
