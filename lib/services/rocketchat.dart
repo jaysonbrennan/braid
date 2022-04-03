ac965c3c5d696167bf9064ab66f45cf017db01be
Jayson Brennan <jaysonbrennan@protonmail.com>
2022-04-03 17:12:41 -0400

    Moving JSON parsing to model

--- lib/services/rocketchat.dart
+++ lib/services/rocketchat.dart
@@ -5,33 +5,6 @@ import 'package:http/http.dart' as http;
 
 import '../user/user.dart';
 
-User parseUser(Map<String, dynamic> json) {
-  final requestStatus = json['status'];
-  if (requestStatus == 'success') {
-    final userId = json['userId'] as String;
-    final me = json['me'];
-    final username = me['name'] as String;
-    final emails = me['emails']
-        .map<String>((email) => (email['address'] as String))
-        .toList();
-    final status = me['status'] as String;
-    final utcOffset = me['utcOffset'] as int;
-    final roles = (me['roles'] as List<dynamic>).cast<String>();
-    final avatar = me['avatarUrl'];
-
-    return User(
-        username: username,
-        userId: userId,
-        emails: emails,
-        status: status,
-        utcOffset: utcOffset,
-        roles: roles,
-        avatar: avatar);
-  } else {
-    throw Exception('User JSON status not successful');
-  }
-}
-
 Future<User> login(http.Client client,
     {required String host,
     required String username,
@@ -40,7 +13,7 @@ Future<User> login(http.Client client,
   final response =
       await client.post(url, body: {'user': username, 'password': password});
   if (response.statusCode == 200) {
-    return parseUser(jsonDecode(response.body));
+    return User.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to login. Unsuccessful status code returned');
   }
--- lib/user/user.dart
+++ lib/user/user.dart
@@ -1,18 +1,51 @@
-class User {
-  final String username;
-  final String userId;
-  late List<String> emails;
-  final List<String> roles;
-  final String status;
-  final int utcOffset;
-  final String avatar;
+import 'package:flutter/foundation.dart';
+
+class User with ChangeNotifier {
+  final String? username;
+  final String? userId;
+  final List<String?>? emails;
+  final List<String>? roles;
+  String status;
+  int utcOffset;
+  String avatar;
+
+  bool loggedIn = false;
 
   User(
-      {required this.username,
-      required this.userId,
-      required this.emails,
-      required this.roles,
-      this.status = "offline",
+      {this.username,
+      this.userId,
+      this.emails,
+      this.roles,
+      this.status = 'offline',
       this.utcOffset = 0,
-      this.avatar = ""});
+      this.avatar = ''});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    final requestStatus = json['status'];
+    if (requestStatus == 'success') {
+      final userId = json['userId'] as String?;
+      final me = json['me'];
+      final username = me['name'] as String?;
+      final emails = (me['emails'] as List<dynamic>?)
+          ?.map((email) => (email['address'] as String?))
+          .toList();
+      final status = me['status'] as String?;
+      final utcOffset = me['utcOffset'] as int?;
+      final roles = (me['roles'] as List<dynamic>?)
+          ?.map((role) => role as String)
+          .toList();
+      final avatar = me['avatarUrl'] as String?;
+
+      return User(
+          username: username,
+          userId: userId,
+          emails: emails,
+          roles: roles,
+          status: status ?? 'offline',
+          utcOffset: utcOffset ?? 0,
+          avatar: avatar ?? '');
+    } else {
+      return User();
+    }
+  }
 }
