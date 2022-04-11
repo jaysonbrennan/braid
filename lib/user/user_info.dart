662adde985102fc15193cbc29806c392c28e7dc8
Jayson Brennan <jaysonbrennan@protonmail.com>
2022-04-10 20:50:12 -0400

    Adding class for updating user info

---
+++ lib/user/user_info.dart
@@ -0,0 +1,32 @@
+enum AccountRole {
+  user,
+  admin,
+}
+
+enum OnlineStatus {
+  offline,
+  online,
+  away,
+}
+
+class UserInfo {
+  final String username;
+  final String userId;
+  final List<AccountRole> roles;
+  final String authToken;
+  List<String> emails;
+  OnlineStatus status;
+  int utcOffset;
+  String avatar;
+
+  UserInfo({
+    required this.username,
+    required this.userId,
+    required this.roles,
+    required this.authToken,
+    emails,
+    this.status = OnlineStatus.offline,
+    this.utcOffset = 0,
+    this.avatar = '',
+  }) : emails = emails ?? List<String>.empty();
+}
