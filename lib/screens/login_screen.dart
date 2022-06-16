cf460c9d0490f50afce0bb6f51cf8cb279635c64
Jayson Brennan <jaysonbrennan@protonmail.com>
2022-06-16 10:26:33 -0400

    Refactoring login elements into their own widget

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,76 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../user/user.dart';
+import '../widgets/login_widget.dart';
 
 class LoginScreen extends StatelessWidget {
-  LoginScreen({Key? key}) : super(key: key);
-  final _urlController = TextEditingController();
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-  var _displayLoginError = false;
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: _urlController,
-              decoration: const InputDecoration(labelText: 'Url'),
-            ),
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: TextButton(
-                onPressed: () {
-                  var user = context.read<User>();
-                  _login(
-                    user,
-                    host: _urlController.text,
-                    username: _usernameController.text,
-                    password: _passwordController.text,
-                  );
-                },
-                child: const Text('Log In'),
-              ),
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12.0),
+          child: LoginWidget(),
         ),
       ),
-      //backgroundColor: const Color(0xFF424242), // dark grey
     );
-  }
-
-  void _login(
-    User user, {
-    required String host,
-    required String username,
-    required String password,
-  }) async {
-    final success =
-        await user.login(host: host, username: username, password: password);
-    _onLoginAttemptComplete(success);
-  }
-
-  void _onLoginAttemptComplete(bool success) {
-    if (success) {
-      _urlController.text = '';
-      _usernameController.text = '';
-    }
-
-    _displayLoginError = !success;
-    _passwordController.text = '';
   }
 }
---
+++ lib/widgets/login_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../user/user.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  final _urlController = TextEditingController();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  var _displayLoginError = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TextField(
+          controller: _urlController,
+          decoration: const InputDecoration(labelText: 'Url'),
+        ),
+        TextField(
+          controller: _usernameController,
+          decoration: const InputDecoration(labelText: 'Username'),
+        ),
+        TextField(
+          controller: _passwordController,
+          decoration: const InputDecoration(labelText: 'Password'),
+          obscureText: true,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextButton(
+            onPressed: () {
+              var user = context.read<User>();
+              _login(
+                user,
+                host: _urlController.text,
+                username: _usernameController.text,
+                password: _passwordController.text,
+              );
+            },
+            child: const Text('Log In'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _login(
+    User user, {
+    required String host,
+    required String username,
+    required String password,
+  }) async {
+    final success =
+        await user.login(host: host, username: username, password: password);
+    _onLoginAttemptComplete(success);
+  }
+
+  void _onLoginAttemptComplete(bool success) {
+    if (success) {
+      _urlController.text = '';
+      _usernameController.text = '';
+    }
+
+    _displayLoginError = !success;
+    _passwordController.text = '';
+  }
+}
